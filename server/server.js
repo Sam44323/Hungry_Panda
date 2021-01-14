f43361c34b4980b7df0e0b6fba585bb7ed2c55f9
Suranjan <suranjanbiswas2002@gmail.com>
2021-01-14 22:08:25 +0530

    Added the basic server functionality and and installed the express body-parser packages for the server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
-console.log('This is the server');
+const express = require('express');
+const bodyparser = require('body-parser');
+
+const app = express();
+app.use(bodyparser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  console.log('This is working');
+});
+
+app.listen(3000);
